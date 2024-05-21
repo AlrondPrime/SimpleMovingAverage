83692ca888df6068f2471ecb7eea25a0894d1203
AlrondPrime <AlrondPrime@gmail.com>
2024-05-21 19:12:18 +0300

    Implement SMA algorithm

---
+++ include/math.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+
+namespace math
+{
+    // Constants
+    constexpr uint32_t gDesiredPrecision = 3; // digits after point
+
+    /**
+     * @tparam T floating point type
+     * @param v value to round
+     * @param desiredPrecision how many significant digits should be left after the point
+     * @return rounded value
+     */
+    template<typename T>
+    double round(T v, uint32_t desiredPrecision = gDesiredPrecision)
+    {
+        return std::round(v * std::pow(10, desiredPrecision)) / std::pow(10, desiredPrecision);
+    }
+
+    template<typename T>
+    struct Point {
+        using value_t = T;
+        T x{}, y{};
+
+    protected:
+        Point() = default;
+    };
+
+    template<typename T>
+    bool operator==(const Point<T> &left, const Point<T> &right)
+    {
+        return round(left.x) == round(right.x) && round(left.y) == round(right.y);
+    }
+
+    template<typename T>
+    bool operator!=(const Point<T> &left, const Point<T> &right)
+    {
+        return !(left==right);
+    }
+
+    using PointF = Point<float>;
+    using PointD = Point<double>;
+
+    /**
+     * @brief count simple moving average
+     * @details https://en.wikipedia.org/wiki/Moving_average#Simple_moving_average
+     */
+    template<typename T>
+    std::vector<Point<T>>
+    sma(const std::vector<Point<T>> &coordinates, const uint32_t windowSize)
+    {
+        // TODO: add boundary checks
+        std::vector<Point<T>> averages;
+        averages.reserve(coordinates.size() / windowSize);
+        typename Point<T>::value_t average;
+
+        // Count first SMA
+        average = 0;
+        for (size_t i{}; i < windowSize; ++i)
+        {
+            average += coordinates[i].y;
+        }
+        average /= windowSize;
+        averages.emplace_back(Point<T>{coordinates[windowSize - 1].x, average});
+
+        // Count remaining SMAs using previous counted value
+        for (size_t i{1}; i < coordinates.size() - windowSize + 1; ++i)
+        {
+            average = averages.back().y - coordinates[i - 1].y / windowSize
+                    + coordinates[i + windowSize - 1].y / windowSize;
+            averages.emplace_back(Point<T>{coordinates[i + windowSize - 1].x, average});
+        }
+
+        return averages;
+    }
+
+} // namespace math
--- source/main.cpp
+++ source/main.cpp
@@ -1,36 +1,45 @@
+#include <cassert>
+#include <iomanip>
 #include <iostream>
+#include <thread>
 #include <vector>
-#include <random>
 
-struct PointF
+#include "math.hpp"
+
+template<typename T>
+std::string to_string(const std::vector<math::Point<T>> &vec)
 {
-    float x{}, y{};
-};
-
-struct PointD
-{
-    double x{}, y{};
-};
-
-float getRandomF()
-{
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<float> dist(std::numeric_limits<float>::min(),std::numeric_limits<float>::max());
-
-    return dist(gen);
+    std::ostringstream os;
+    os << std::fixed;
+    os << std::setprecision(3);
+    for (const auto &p: vec)
+    {
+        os << "x:" << p.x << ", y:" << p.y << "\n";
+    }
+    return os.str();
 }
 
-double getRandomD()
+int main()
 {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<double> dist(std::numeric_limits<double>::min(),std::numeric_limits<double>::max());
+    // clang-format off
+    std::vector<math::Point<double>> dataset{
+        {1, 34},
+        {2, 32},
+        {3, 37},
+        {4, 33},
+        {5, 38},
+        {6, 35},
+        {7, 39},
+        {8, 34},
+        {9, 39},
+        {10, 35},
+        {11, 37},
+        {12, 32},
+    };
+    // clang-format on
 
-    return dist(gen);
-}
-
-int main() {
+    const auto result = math::sma(dataset, 3);
+    std::cout << to_string(result) << std::endl;
 
     return EXIT_SUCCESS;
 }
